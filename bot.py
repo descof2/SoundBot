5dfc439da8434c9366a3ccc572088194821aec1f
Darell Escofe <darellescofe@gmail.com>
2020-09-08 23:12:37 -0500

    updated !help to reflect new commands

--- bot.py
+++ bot.py
@@ -57,7 +57,9 @@ async def print_commands(message):
                                "!help sounds - list all usable sounds \n"
                                "!jackboxtime - Move everyone to jackbox channel. Include names after the command to exclude moving those users [COMING SOON]\n"
                                "!generaltime - Move everyone from jackbox back to general\n"
-                               "!poll [Question] - Creates a poll for users to vote on")
+                               "!poll [Question] - Creates a poll for users to vote on\n"
+                               "!joey - Links a random JoeysWorldTourVideo:pizza:\n"
+                               "!chugs - Links a random BadlandsChugs:beverage_box:\n")
 
 
 # Print all the sounds the bot can play
